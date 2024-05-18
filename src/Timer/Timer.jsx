6cbe6aab64ffd5512e8d35d72678e780350d3766
Gabriel de Jesus <gabrieldejesus9905@gmail.com>
2024-05-18 11:16:11 -0300

    finish stopwatch-app

--- src/Timer/Timer.jsx
+++ src/Timer/Timer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
 
@@ -32,21 +32,69 @@ const CustomButton = styled(Button)(({ theme }) => ({
 }));
 
 function Timer() {
+
+    const [isRunning, setIsRunning] = useState(false);
+    const [elapsedTime, setElapsedTime] = useState(0);
+    const intervalIdRef = useRef(null);
+    const startTimeRef = useRef(0);
+
+    useEffect(() =>{
+
+        if(isRunning){
+            intervalIdRef.current = setInterval(() => {
+                setElapsedTime(Date.now() - startTimeRef.current);
+            }, 10);
+        }
+
+        return () => {
+            clearInterval(intervalIdRef.current);
+        }
+    }, [isRunning]);
+
+    function start(){
+        setIsRunning(true);
+        startTimeRef.current = Date.now() - elapsedTime;
+    }
+
+    function stop(){
+        setIsRunning(false);
+    }
+
+    function reset(){
+        setElapsedTime(0);
+        setIsRunning(false);
+    }
+
+    function formatTime(){
+        
+        let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+        let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+        let seconds = Math.floor(elapsedTime / (1000) % 60);
+        let milliSeconds = Math.floor((elapsedTime % 1000) / 10);
+
+        hours = String(hours).padStart(2, "0");
+        minutes = String(minutes).padStart(2, "0");
+        seconds = String(seconds).padStart(2, "0");
+        milliSeconds = String(milliSeconds).padStart(2, "0");
+
+        return `${minutes}:${seconds}:${milliSeconds}`;
+    }
+
     return (
         <div className="container-clock">
-            <div className="timer-title">
-                Timer
+            <div className="display">
+                {formatTime()}
             </div>
             <div className="control-clock">
-                <CustomButton variant="contained" color="success">
+                <CustomButton onClick={start} variant="contained" color="success">
                     Start
                 </CustomButton>
 
-                <CustomButton variant="contained" color="error">
+                <CustomButton onClick={stop} variant="contained" color="error">
                     Stop
                 </CustomButton>
 
-                <CustomButton variant="contained" color="secondary">
+                <CustomButton onClick={reset} variant="contained" color="secondary">
                     Reset
                 </CustomButton>
             </div>
